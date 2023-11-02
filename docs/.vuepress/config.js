6de62151f560dfae3ba49f6fbb40705c265acc07
wangyuhan <wangyuhan@hunshehui.cn>
2023-11-02 16:03:15 +0800

    feat: 文章更新

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -77,12 +77,11 @@ module.exports = {
             { text: '关于', link: '/about/' },
             {
                 text: '收藏',
-                link: '/pages/beb6c0bd8a66cea6/',
-                // items: [
-                //   { text: '网站', link: '/pages/beb6c0bd8a66cea6/' },
-                //   { text: '资源', link: '/pages/eee83a9211a70f9d/' },
-                //   { text: 'Vue资源', link: '/pages/12df8ace52d493f6/' },
-                // ],
+                link: '/collect/',
+                items: [
+                  { text: '个人产出', link: '/collect/#个人产出' },
+                  { text: '实用工具', link: '/collect/#实用工具' },
+                ],
             },
             {
                 text: '索引',
